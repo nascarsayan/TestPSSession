2d050ee738819f7041e6018fd3aef55d3fe91dee
ArcVMwareUser <arcvmwareuser@microsoft.com>
2022-06-20 10:05:12 +0530

    simple

--- Runner.cs
+++ Runner.cs
@@ -1,28 +1,44 @@
-﻿// string hostname = "10.225.91.184", username = "cdmlab\\cdmlabuser", passwd = "!!123abc";
-string hostname = "10.150.170.215", username = "Administrator", passwd = "Password~1";
-ConnectionDto connDto = new ConnectionDto(hostname, username, passwd, 0);
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Security;
 
-string script;
+string hostname = Environment.GetEnvironmentVariable("PSTEST_HOSTNAME") ?? throw new Exception("Hostname not set. Please set $env:PSTEST_HOSTNAME");
+string username = Environment.GetEnvironmentVariable("PSTEST_USERNAME") ?? throw new Exception("Username not set. Please set $env:PSTEST_USERNAME");
+string passwd = Environment.GetEnvironmentVariable("PSTEST_PASSWORD") ?? throw new Exception("Password not set. Please set $env:PSTEST_PASSWORD");
+string ShellUri = "http://schemas.microsoft.com/powershell/Microsoft.PowerShell";
 
-var iter = 1;
-while (true)
+for (int i = 0; i < 1000000; i++)
 {
-  Console.WriteLine("Starting process");
-  Thread.Sleep(2000);
-
-  using  var session = new Session(connDto);
-  for (var i = 1; i <= 20; i++)
+  var securePasswd = new PSCredential(username, StringToSecureString(passwd));
+  var connectionInfo = new WSManConnectionInfo(
+    false, hostname, 5985, "/wsman", ShellUri, securePasswd, 1 * 60 * 1000)
   {
-    var jobCount = 100 + i * 10;
-    // script = $"Clear-Host; $x = Find-SCJob -MaxCount {jobCount} -VMMServer localhost | ConvertTo-Json";
-    script = $"ipconfig | ConvertTo-Json";
-    var x = await session.Execute(script);
-    Console.WriteLine(x.Length);
-    Thread.Sleep(10);
-    Console.WriteLine($"loop = {iter} job-count = {jobCount}");
-    x = null;
-    script = "";
+    AuthenticationMechanism = AuthenticationMechanism.Negotiate,
+    SkipCACheck = true,
+    SkipCNCheck = true,
+  };
+  using var runspace = RunspaceFactory.CreateRunspace(connectionInfo);
+  runspace.Open();
+  using var pwsh = PowerShell.Create(RunspaceMode.NewRunspace);
+  pwsh.Runspace = runspace;
+  // pwsh.AddScript("echo 'a'");
+  pwsh.AddScript("$i = 0");
+  var x = pwsh.Invoke();
+  foreach (var y in x) {
+    Console.WriteLine(y.ToString().Length);
   }
-  iter++;
-  Console.WriteLine("Disposed session");
+  x.Clear();
+  pwsh.Dispose();
+  runspace.Close();
+  runspace.Dispose();
+  Console.WriteLine($"i = {i}");
+}
+
+SecureString StringToSecureString(string password)
+{
+  if (password == null)
+    throw new ArgumentNullException(nameof(password));
+  var securePassword = new SecureString();
+  password.ToList().ForEach(x => securePassword.AppendChar(x));
+  return securePassword;
 }
