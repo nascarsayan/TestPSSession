e894bfff49426f69423555cbc1860d66a77d19a8
ArcVMwareUser <arcvmwareuser@microsoft.com>
2022-06-23 20:31:08 +0530

    multiple runspaces on same process not working

--- Runner.cs
+++ Runner.cs
@@ -1,6 +1,8 @@
+using System.Collections.Concurrent;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Security;
+using System.Text;
 
 string hostname = Environment.GetEnvironmentVariable("PSTEST_HOSTNAME") ?? throw new Exception("Hostname not set. Please set $env:PSTEST_HOSTNAME");
 string username = Environment.GetEnvironmentVariable("PSTEST_USERNAME") ?? throw new Exception("Username not set. Please set $env:PSTEST_USERNAME");
@@ -9,59 +11,78 @@ string passwd = Environment.GetEnvironmentVariable("PSTEST_PASSWORD") ?? throw n
 
 Console.WriteLine($"Hostname = '{hostname}' Username = '{username}' Password = '{passwd}'");
 
-List<PSObject> RunLocalCommand(Runspace runspace, Command arbitraryCommand)
-{
-  using (var powershell = PowerShell.Create())
-  {
-    powershell.Runspace = runspace;
-
-    powershell.Commands.AddCommand(arbitraryCommand);
-
-    var output = powershell.Invoke();
-
-    // ThrowOnError(powershell, arbitraryCommand.CommandText, "localhost");
-
-    var ret = output.ToList();
-    return ret;
-  }
-}
-
 for (int i = 0; i < 1000; i++)
 {
   using PowerShellProcessInstance pwshProcess = new PowerShellProcessInstance();
   pwshProcess.Process.StartInfo.FileName = @"/usr/bin/pwsh";
   pwshProcess.Process.Start();
   var creds = new PSCredential(username, StringToSecureString(passwd));
-  using var runspace = RunspaceFactory.CreateOutOfProcessRunspace(new TypeTable(new string[0]), pwshProcess);
-  runspace.Open();
-  //
-  string[] script = {"hostname", "ipconfig", "pwd"};
+  string[] script = { "hostname", "ipconfig", "pwd" };
 
-  using var pwsh = PowerShell.Create(RunspaceMode.NewRunspace);
-  pwsh.Runspace = runspace;
-  runspace.SessionStateProxy.SetVariable("creds", creds);
-  var fullScript = 
-    "$so = New-PSSessionOption -SkipCACheck -SkipCNCheck; "+
-    "$session = New-PSSession -ComputerName " + hostname + " -Authentication 'negotiate' -Credential $creds -SessionOption $so;";
-  Console.WriteLine(fullScript);
-  pwsh.AddScript(fullScript);
-  var x = pwsh.Invoke();
-  x.Clear();
-  for (int j = 0; j < 50; j++) {
-    fullScript = "Invoke-Command -Session $session -ScriptBlock { " + script[j%3] + " };";
-    Console.WriteLine(fullScript);
-    pwsh.AddScript(fullScript);
-    x = pwsh.Invoke();
-    // foreach (var y in x)
-    // {
-    //   Console.WriteLine(y.ToString());
-    // }
-    x.Clear();
-    ThrowOnError(pwsh, script[j%3]);
+  var rsList = new List<Runspace>();
+  for (int j = 0; j < script.Length; j++)
+  {
+    var runspace = RunspaceFactory.CreateOutOfProcessRunspace(new TypeTable(new string[0]), pwshProcess);
+    runspace.Open();
+    Console.WriteLine("Created runspace " + j);
+    // The second runspace creation is failing.
+    rsList.Add(runspace);
   }
-  pwsh.Dispose();
-  runspace.Close();
-  runspace.Dispose();
+  Console.WriteLine("Created runspaces");
+
+  var tasks = new List<Task>();
+
+  var outputs = new ConcurrentBag<string>();
+
+  for (int j = 0; j < script.Length; j++)
+  {
+    tasks.Add(Task.Run(() =>
+    {
+      using var pwsh = PowerShell.Create(RunspaceMode.NewRunspace);
+      pwsh.Runspace = rsList[j];
+      rsList[j].SessionStateProxy.SetVariable("creds", creds);
+      var fullScript =
+        "$so = New-PSSessionOption -SkipCACheck -SkipCNCheck; " +
+        "$session = New-PSSession -ComputerName " + hostname + " -Authentication 'negotiate' -Credential $creds -SessionOption $so;";
+      Console.WriteLine(fullScript);
+      pwsh.AddScript(fullScript);
+      var invokeRes = pwsh.Invoke();
+      invokeRes.Clear();
+      fullScript = "Invoke-Command -Session $session -ScriptBlock { " + script[j % 3] + " };";
+      Console.WriteLine(fullScript);
+      pwsh.AddScript(fullScript);
+      invokeRes = pwsh.Invoke();
+      StringBuilder sb = new StringBuilder();
+      foreach (var y in invokeRes)
+      {
+        sb.AppendLine(y.ToString());
+      }
+      outputs.Add(sb.ToString());
+      sb.Clear();
+      invokeRes.Clear();
+      ThrowOnError(pwsh, script[j % 3]);
+      pwsh.Dispose();
+    }));
+  }
+
+  Task t = Task.WhenAll(tasks);
+  try
+  {
+    Console.WriteLine("Waiting for tasks to complete");
+    t.Wait();
+  }
+  catch { }
+  if (t.Status == TaskStatus.RanToCompletion)
+    Console.WriteLine("All tasks succeeded.");
+  else if (t.Status == TaskStatus.Faulted)
+    Console.WriteLine("Some tasks failed");
+
+  for (int j = 0; j < script.Length; j++)
+  {
+    rsList[j].Close();
+    rsList[j].Dispose();
+  }
+
   Console.WriteLine($"i = {i}");
   if (i == 10 || i == 500)
   {
